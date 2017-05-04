5a64579b36a98c753d2f4531e2c36ecb6199eeea
KREMER Michel <michel.kremer.003@student.uni.lu>
2017-05-04 12:05:44 +0200

    Adapted Implementation and messir Specification of class types

--- lu.uni.lassy.excalibur.examples.icrash.dev.desktop.java.commons/src/lu/uni/lassy/excalibur/examples/icrash/dev/java/system/types/primary/CtCaptcha.java
+++ lu.uni.lassy.excalibur.examples.icrash.dev.desktop.java.commons/src/lu/uni/lassy/excalibur/examples/icrash/dev/java/system/types/primary/CtCaptcha.java
@@ -5,18 +5,21 @@ import java.io.Serializable;
 import lu.uni.lassy.excalibur.examples.icrash.dev.java.system.types.design.JIntIs;
 import lu.uni.lassy.excalibur.examples.icrash.dev.java.types.stdlib.DtString;
 import lu.uni.lassy.excalibur.examples.icrash.dev.java.types.stdlib.PtBoolean;
+import lu.uni.lassy.excalibur.examples.icrash.dev.java.types.stdlib.PtInteger;
 import lu.uni.lassy.excalibur.examples.icrash.dev.java.types.stdlib.PtString;
 
-public class DtCaptcha implements Serializable, JIntIs{//TODO: Complete this class (Images)
+public class CtCaptcha implements Serializable, JIntIs{//TODO: Complete this class (Images)
 	
 	private static final long serialVersionUID = 7612248309713646319L;
 
 	private DtCaptchaId id;
 	private DtString question;
+	private CtCaptchaImage images[];
 	
-	public DtCaptcha(DtCaptchaId id, PtString question){
+	public CtCaptcha(DtCaptchaId id, PtString question, CtCaptchaImage images[]){
 		this.id = id;
 		this.question = new DtString(question);
+		this.images = images;
 	}
 
 	@Override
@@ -30,6 +33,10 @@ public class DtCaptcha implements Serializable, JIntIs{//TODO: Complete this cla
 	
 	public DtString getQuestion(){//TODO: Messir
 		return question;
+	}
+	
+	public CtCaptchaImage getImageAt(PtInteger index){
+		return images[index.getValue()];
 	}
 
 }
